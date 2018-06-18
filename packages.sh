c5bc15efb18464e4bede6ecf1d1986474b36a179
ev.ynam.w@gmail.com <Evelyn@EVELYN-sMAC.local>
2018-06-18 11:34:40 +0800

    added LS EW199

--- packages.sh
+++ packages.sh
@@ -2,3 +2,4 @@
 dpkg-deb -bZgzip projects/system/repoicons debs
 dpkg-deb -bZgzip projects/others/ew1pagedots debs
 dpkg-deb -bZgzip projects/LS/lsew198 debs
+dpkg-deb -bZgzip projects/LS/lsew199 debs
