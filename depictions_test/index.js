3a8a17648d2dcedad18e3a5dadb84412f176abfb
evynw <ev.ynam.w@gmail.com>
2019-03-24 19:07:27 +0800

    testing ios checker

--- depictions_test/index.js
+++ depictions_test/index.js
@@ -78,6 +78,7 @@ $(document).ready(function () {
 
         // iOS version check
         var currentVersion = iOSVersion();
+        var otherVersion = data.otheriOS;
         if (typeof currentVersion === 'undefined' &&
             (typeof data.minOSVersion !== 'undefined' || typeof(data.maxOSVersion) !== 'undefined')) {
             var result = "<strong>Compatible with iOS ";
@@ -100,11 +101,15 @@ $(document).ready(function () {
                 result += "Your iOS version (" + currentVersion + ") is <strong>compatible</strong> &#x1f607;";
                 // $(".version-check").css("color", "green");
                 $(".panel-body.version-check").css("background-color", "#52d183");
-            } else{
-                result += "<strong>Not confirmed</strong> to work on your iOS version";
-                result += (typeof currentVersion != 'undefined') ? " (" + currentVersion + ")" : "";
-                result += ".";
-                $(".panel-body.version-check").css("background-color", "#ffe02b");
+            } else {
+                if (typeof otheriOS == "untested"){
+                    result += "<strong>Not confirmed</strong> to work on your iOS version";
+                    result += (typeof currentVersion != 'undefined') ? " (" + currentVersion + ")" : "";
+                    result += ".";
+                    $(".panel-body.version-check").css("background-color", "#ffe02b");
+                } else if (typeof otheriOS == "unsupported"){
+                    result += "Only compatible with iOS " + data.minOSVersion + " to " + data.maxOSVersion + " &#x1F61F;";
+                }
             }
             $(".version-check").html(result);
         }
